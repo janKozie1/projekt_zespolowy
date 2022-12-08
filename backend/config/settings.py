8caa88e24f971fdc3dbe02d8e738db0c1d7772fc
Tomasz Macałka <tomasz.macalka@student.pk.edu.pl>
2022-12-08 21:15:49 +0100

    Modified MySQL config, removed SQLite, new requirements and ignores

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from config.secrets import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -75,17 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-
-    'giftplanner_db': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'giftplanner_db',
-        'USER': 'SomeUser',
-        'PASSWORD': 'SecurePassword',
-        'HOST': 'SomeHost',
-        'PORT': '3306',
+        'NAME': db_name,
+        'USER': db_user,
+        'PASSWORD': db_pass,
+        'HOST': db_host,
+        'PORT': db_port,
     }
 }
 
