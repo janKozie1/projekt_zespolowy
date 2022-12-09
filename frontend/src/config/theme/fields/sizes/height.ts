d62e02242c0fae690e4bff929f50ba834775cadf
Damian <kuciel.damian2@gmail.com>
2022-12-09 10:31:52 +0100

    Product page

--- frontend/src/config/theme/fields/sizes/height.ts
+++ frontend/src/config/theme/fields/sizes/height.ts
@@ -1,6 +1,9 @@
 const height = {
   calendarCell: '44px',
   discoveryCard: '275px',
+  productImage: '569px',
+  smallImage: '110px',
+  homeImage: '1040px',
 } as const;
 
 export type Height = typeof height;
--- frontend/src/config/theme/fields/sizes/width.ts
+++ frontend/src/config/theme/fields/sizes/width.ts
@@ -2,6 +2,9 @@ const width = {
   sidebar: '290px',
   calendarCell: '44px',
   discoveryCard: '180px',
+  productImage: '616px',
+  smallImage: '139px',
+  homeImage: '1490px',
 } as const;
 
 export type Width = typeof width;
